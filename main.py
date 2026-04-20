10e074650af3f7007cf2a0c8b3facd9529fa9275
Binary0_0Bytes <tarunmawri2005@gmail.com>
2026-04-21 01:28:25 +0530

    origin update

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["POST", "OPTIONS"],
+    allow_headers=["Content-Type"],
 )
 
 try:
